a179313c097c79391b5f209351a1668e0c9e21e1
hartforda <hartforda@msoe.edu>
2018-04-19 11:29:17 -0500

    Added multi-threaded implementations.  Currently they are slower than single-thread.  Awesome! -.-

--- CS3851Lab4/src/mMergeSort.java
+++ CS3851Lab4/src/mMergeSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Multi-Threaded MergeSort
  * @author Alex Hartford
@@ -10,6 +12,69 @@ public class mMergeSort implements Algorithm {
 
     @Override
     public String sort(int[] A) {
-        return null;
+        long startTime = System.nanoTime();
+        sort(A, 0, A.length - 1);
+        System.out.println("[MergeSort] Sorted Array: " + Arrays.toString(A));
+        return String.valueOf(System.nanoTime() - startTime);
+    }
+
+    private void merge(int A[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        for (int i = 0; i < n1; i++) L[i] = A[l + i];
+        for (int i = 0; i < n2; i++) R[i] = A[m + i + 1];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            }
+            else {
+                A[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            A[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            A[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private void sort(int A[], int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            Thread t1 = new Thread(() -> sort(A, l, m));
+            Thread t2 = new Thread(() -> sort(A , m + 1, r));
+
+            t1.start();
+            t2.start();
+
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                System.out.println("[MergeSort] Threads failed to join");
+                e.printStackTrace();
+            }
+
+            merge(A, l, m, r);
+        }
     }
 }
--- CS3851Lab4/src/mQuickSort.java
+++ CS3851Lab4/src/mQuickSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Multi-Threaded QuickSort
  * @author Alex Hartford
@@ -9,7 +11,49 @@
 public class mQuickSort implements Algorithm {
 
     @Override
-    public String sort(int[] A) {
-        return null;
+    public String sort(int A[]) {
+        long startTime = System.nanoTime();
+        quicksort(A, 0, A.length - 1);
+        System.out.println("[QuickSort] Sorted Array: " + Arrays.toString(A));
+        return String.valueOf(System.nanoTime() - startTime);
+    }
+
+    private int partition(int A[], int low, int high) {
+        int pivot = A[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (A[j] <= pivot) {
+                i++;
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+
+        int temp = A[i + 1];
+        A[i + 1] = A[high];
+        A[high] = temp;
+
+        return i + 1;
+    }
+
+    private void quicksort(int A[], int low, int high) {
+        if (low < high) {
+            int i = partition(A, low, high);
+
+            Thread t1 = new Thread(() -> quicksort(A, low, i - 1));
+            Thread t2 = new Thread(() -> quicksort(A, i + 1, high));
+
+            t1.start();
+            t2.start();
+
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                System.out.println("[QuickSort] Threads failed to join");
+                e.printStackTrace();
+            }
+        }
     }
 }
